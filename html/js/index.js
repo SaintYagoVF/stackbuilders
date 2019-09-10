a7467e8b27b2bcaf48876a23a0eaa9f2a1b4dd29
SaintYagoVF <safvillacis@udlanet.ec>
2019-09-09 21:28:12 -0500

    Time Input and method
    JavaScript methods, sweet alert message

--- html/js/index.js
+++ html/js/index.js
@@ -1,3 +1,45 @@
 function predictMethod() {
+    var timePicked=document.getElementById("time_picker").value;
+    var platePicked=document.getElementById("plateNumber").value;
+    var dayWeek = new Date(dayPicked);
+    console.log(dayPicked);
+    console.log(timePicked);
+    console.log(dayWeek.getDay());
+    console.log(platePicked);
+
+    switch(dayWeek.getDay()) {
+        case 1:
+        createSwalYes();
+          break;
+        case 2:
+            createSwalYes();
+          break;
+        default:
+            createSwalNo();
+      }
 
 }
+
+function createSwalYes() {
+
+       swal({
+         title: "Car can be on road!",
+         icon: "success",
+         button: "Accept",
+       });
+      
+    
+   }
+
+   function createSwalNo() {
+    swal({
+      
+        title: "Car can't be on road!",
+        icon: "error",
+        button: "Accept",
+      });
+
+}
+
+
+
